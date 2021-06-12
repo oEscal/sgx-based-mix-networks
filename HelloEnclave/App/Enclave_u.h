7a26b760f273ba13980b2d5a817592d2bd8c720b
oEscal <pedroescaleira@hotmail.com>
2021-06-12 17:44:47 +0100

    small fixes to the repository tree

---
+++ HelloEnclave/App/Enclave_u.h
@@ -0,0 +1,33 @@
+#ifndef ENCLAVE_U_H__
+#define ENCLAVE_U_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include <string.h>
+#include "sgx_edger8r.h" /* for sgx_status_t etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef OCALL_PRINT_STRING_DEFINED__
+#define OCALL_PRINT_STRING_DEFINED__
+void SGX_UBRIDGE(SGX_NOCONVENTION, ocall_print_string, (const char* str));
+#endif
+
+sgx_status_t create_keys(sgx_enclave_id_t eid, unsigned char* p_n);
+sgx_status_t import_message(sgx_enclave_id_t eid, unsigned char* message);
+sgx_status_t set_public_key(sgx_enclave_id_t eid, unsigned char* module);
+sgx_status_t dispatch(sgx_enclave_id_t eid, unsigned char* result, int* fan_out, size_t* buffer_size, int fan_all_out);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
